f6e8426211b60b5db835d8ab2d50bd3d6d9eba22
famo1245 <kandallee38@gmail.com>
2023-11-02 01:32:06 +0900

    Studying p.127

--- nest_study/src/users/dto/create-user.dto.ts
+++ nest_study/src/users/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { Transform } from "class-transformer";
+import { BadRequestException } from "@nestjs/common";
+import { NotIn } from "./not-in";
 
 export class CreateUserDto {
+  // trim() 메서드가 처리하지 못하는 공백 있음
+  @Transform((params) => params.value.trim())
+  @NotIn("password", { message: "password는 name과 같은 문자열을 포함할 수 없습니다." })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
@@ -11,7 +17,14 @@ export class CreateUserDto {
   @MaxLength(60)
   readonly email: string;
 
+  // @Transform(({ value, obj }) => {
+  //   if (obj.password.includes(obj.name.trim())) {
+  //     throw new BadRequestException("password는 name과 같은 문자열을 포함할 수 없습니다.");
+  //   }
+  //
+  //   return value.trim();
+  // })
   @IsString()
-  @Matches(/^[A-Za-z\d!@#$%^&*()]{8, 30}$/)
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
